0406534e8328379c1ea6fd428fdbbb0a2ba3a42e
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-01-19 10:58:19 -1100

    worked on Navbar and Header

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+
+const Header = () => {
+  return (
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something with GPT-3 OpenAI</h1>
+        <div className='gpt3__header-content__input'>
+          <p>Yet bed any for travelling assistance indulgence unpleasing. <br />
+            Not thoughts all exercise blessing. Indulgence way <br />
+            everything joy alteration boisterous the attachment. <br />
+             Party we years to order allow asked of.</p>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started </button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
